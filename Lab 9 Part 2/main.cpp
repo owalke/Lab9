09c839720b7c27a69483896c5575183c029c9aec
Olivia Walke <olivia@olivias-macbook.home>
2016-11-27 19:54:40 -0700

    Initial Commit

---
+++ Lab 9 Part 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 9 Part 2
+//
+//  Created by Olivia Walke on 11/27/16.
+//  Copyright © 2016 Olivia Walke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
